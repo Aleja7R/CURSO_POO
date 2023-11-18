cbfbeed7e46bcb58d185adeca89aab7e1329035a
Alejandra Rojas Quintero <116470017+Aleja7R@users.noreply.github.com>
2023-11-18 11:45:31 -0500

    Add files via upload
    Ejercicio para calcular promedio, nota mayor y menor y desviacion estandar

---
+++ ACTIVIDAD_5/EjercicioNotas/src/main/java/notas/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package notas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        VentanaPrincipal miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true);
+    }
+}
