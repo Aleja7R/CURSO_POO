4aa6fa20cfafc7c8b2465f728d2491988bff701f
Alejandra Rojas Quintero <116470017+Aleja7R@users.noreply.github.com>
2023-11-26 14:54:54 -0500

    Add files via upload
    Actividad 6

---
+++ ACTIVIDAD_6/Principal/src/main/java/com/mycompany/principal/Principal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.principal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Formulario form = new Formulario(); 
+        form.setVisible(true);
+    }
+}
